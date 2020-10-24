5ea4dc759e5472b8ebeabb2453c7ed35df5c47a4
tuyentac95 <tuyentac95@gmail.com>
2020-10-25 00:16:16 +0700

    listing and creating employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
